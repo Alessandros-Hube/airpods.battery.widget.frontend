3f59e05287881d9301e7cde9e7741c032d065a33
Alessandros-Hube <56445937+Alessandros-Hube@users.noreply.github.com>
2025-02-05 08:47:35 +0100

    fix: use virtual environment and specify bleak~=0.22.3

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+cd ~/.local/share/plasma/plasmoids/airpods.battery.widget.frontend
+
+python3 -m venv .
+
+./bin/pip3 install -r requirements.txt
+
 while true; do
-    python3 ~/.local/share/plasma/plasmoids/airpods.battery.widget.frontend/main.py ~/.local/share/plasma/plasmoids/airpods.battery.widget.frontend/airstatus.out
+    ~/.local/share/plasma/plasmoids/airpods.battery.widget.frontend/bin/python3 ~/.local/share/plasma/plasmoids/airpods.battery.widget.frontend/main.py ~/.local/share/plasma/plasmoids/airpods.battery.widget.frontend/airstatus.out
     sleep 30  # 30 seconds equals 1 minute
 done
